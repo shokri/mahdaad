81b0ef1c08d408621c9eabe5f1a8406908e41093
mrShokri <mrshokri98@gmail.com>
2025-07-19 16:18:01 +0330

    feat: add sticky header and footer with scrollable content area

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-screen flex flex-col">
+    <header class="bg-gray-100 p-4 text-center">Header</header>
+    <main class="flex-1 overflow-y-auto p-4">
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+      <p>Scroll...</p>
+    </main>
+    <footer class="bg-gray-100 p-4 text-center">Footer</footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
